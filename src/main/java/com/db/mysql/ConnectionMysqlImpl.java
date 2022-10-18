d117448606f15d29ac0f5f7b7332b9bb977efaa0
seoyun <may-primavera@naver.com>
2022-10-18 14:20:57 +0900

    refact: 함수명 변경

--- src/main/java/com/db/mysql/ConnectionMysqlImpl.java
+++ src/main/java/com/db/mysql/ConnectionMysqlImpl.java
@@ -6,14 +6,10 @@ import java.util.Map;
 public class ConnectionMysqlImpl implements DbConnector{//TODO: db를 설정을 각각 적용해주면 좋겠다
     private Map<String, String> env;
     private Connection conn;
-    private PreparedStatement ps;
-    private ResultSet rs;
 
     public ConnectionMysqlImpl() throws SQLException {
         this.env = System.getenv();
         this.conn = DriverManager.getConnection(env.get("DB_HOST"), env.get("DB_USER"), env.get("DB_PASSWORD"));
-        this.ps = null;
-        this.rs = null;
     }
 
     @Override
