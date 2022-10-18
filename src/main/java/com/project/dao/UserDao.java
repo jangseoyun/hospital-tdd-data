3dd1d4e4fa79d49f701094cc54d069b04b767c7b
seoyun <may-primavera@naver.com>
2022-10-18 10:17:34 +0900

    refact: select, insert 쿼리문

--- src/main/java/com/project/dao/UserDao.java
+++ src/main/java/com/project/dao/UserDao.java
@@ -18,23 +18,25 @@ public class UserDao {
     ConnectionDB connectionDB = new ConnectionDB();
     Query query = new UserQueryImpl();
 
-    public void add() throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        ps = connectionDB.getConn().prepareStatement(query.add());
-        ps.setInt(1, 1);
-        ps.setString(2, "hello");
-        ps.setString(3, "13323");
-
+    public void add() {
+        try {
+            ps = connectionDB.getConn().prepareStatement(query.add());
+            ps.setInt(1, 1);
+            ps.setString(2, "hello");
+            ps.setString(3, "13323");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void userSelectOne() throws SQLException {
+    public void userFindById(int id) throws SQLException {
         PreparedStatement ps = connectionDB.getConn().prepareStatement(query.findOne());
-        ps.setInt(1, 1);
+        ps.setInt(1, id);
         ResultSet rs = ps.executeQuery();
         while (rs.next()) {
             int getId = rs.getInt(1);
-            String name = rs.getString(2);
-            String password = rs.getString(3);
+            String getName = rs.getString(2);
+            String getPassword = rs.getString(3);
             System.out.println(getId);
         }
 
@@ -44,7 +46,7 @@ public class UserDao {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
         //userDao.add();
-        userDao.userSelectOne();
+        userDao.userFindById(1);
 
     }
 }
