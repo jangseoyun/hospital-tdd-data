b26c822b8ec0eaef947772230212086408c01669
seoyun <may-primavera@naver.com>
2022-10-19 13:39:54 +0900

    feat: spring 적용 후 테스트

--- src/main/java/com/project/dao/UserDaoFactory.java
+++ src/main/java/com/project/dao/UserDaoFactory.java
@@ -2,16 +2,21 @@ package com.project.dao;
 
 import com.db.mysql.ConnectionLocalImpl;
 import com.db.mysql.ConnectionMysqlImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.sql.SQLException;
 
+@Configuration
 public class UserDaoFactory {
+    @Bean
     public UserDao awsUserDao() throws SQLException {
         ConnectionMysqlImpl connectionMysql = new ConnectionMysqlImpl();
         UserDao userDao = new UserDao(connectionMysql);
         return userDao;
     }
 
+    @Bean
     public UserDao localUserDao() throws SQLException {
         ConnectionLocalImpl connectionLocal = new ConnectionLocalImpl();
         UserDao userDao = new UserDao(connectionLocal);
--- src/test/java/com/project/dao/UserDaoTest.java
+++ src/test/java/com/project/dao/UserDaoTest.java
@@ -4,24 +4,33 @@ import com.project.vo.UserVo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 @DisplayName("user DAO 데이터 쿼리 ")
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
 
     @DisplayName("사용자 등록 성공 확인")
     @Test
     void 사용자등록테스트() throws SQLException, ClassNotFoundException {
         //given
-        //UserDao userDao = new UserDao(new ConnectionMysqlImpl());
-        UserDao userDao = new UserDaoFactory().awsUserDao();
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
         UserVo user = new UserVo(2, "hellohahaha", "1234");
         userDao.add(user);
 
-        UserVo selectUserOne = userDao.userFindById(1);
-        //when
-        Assertions.assertEquals("seoyun", selectUserOne.getName());
+        UserVo selectUserOne = userDao.userFindById(2);
+
+        Assertions.assertEquals("hellohahaha", selectUserOne.getName());
     }
 
     @DisplayName("특정 사용자 삭제")
